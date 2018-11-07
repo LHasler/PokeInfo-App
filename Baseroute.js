d2723de4c6b36655e872a757059e702096f79982
LukePatrickHasler <39388864+LHasler@users.noreply.github.com>
2018-11-07 17:22:35 -0500

    Update Baseroute.js

--- Baseroute.js
+++ Baseroute.js
@@ -5,45 +5,45 @@ class Baseroute extends Component {
     super(props);
     this.state = {
         selectedpokemon: null,
-        location: [],
-        type: [],
-        generation: []
+        types: [],
+        species: [],
+        game_indices: []
     }
 }
 
 
     async componentDidMount() {
      const res = 
-        await  fetch(`https://pokeapi.co/api/v2/pokemon/${this.props.match.params.name}/`, {cache: "force-cache"})
+        await  fetch(`https://pokeapi.co/api/v2/${this.props.match.params.name}/`, {cache: "force-cache"})
         const json = await res.json()
 
   
-     
-     const locationPromises = json.location.map(async (l) => {
-      const data = await fetch(l.location.url, {cache: "force-cache"} )
+console.log(types)
+     const typesPromises = json.types.map(async (t) => {
+      const data = await fetch(t.types.url, {cache: "force-cache"} )
       const json = await data.json() 
       return json
     })
 
-  const location = await Promise.all(locationPromises)
+  const types = await Promise.all(typesPromises)
   
-  const typePromises = json.type.map(async (t) => {
-    const data = await fetch(t.type.url, {cache: "force-cache"} )
+  const speciesPromises = json.species.map(async (s) => {
+    const data = await fetch(s.species.url, {cache: "force-cache"} )
     const json = await data.json() 
     return json
   })
 
-  const type = await Promise.all(typePromises)
+  const species = await Promise.all(speciesPromises)
 
-  const generationPromises = json.generation.map(async (g) => {
-    const data = await fetch(g.generation.url, {cache: "force-cache"} )
+  const game_indicesPromises = json.game_indices.map(async (g) => {
+    const data = await fetch(g.game_indices.url, {cache: "force-cache"} )
     const json = await data.json() 
     return json
   })
 
-  const generation = await Promise.all(generationPromises)
+  const game_indices = await Promise.all(game_indicesPromises)
 
-  this.setState({selectedPokemon: json, location: location, type: type, generation: generation})
+  this.setState({selectedPokemon: json, game_indices: game_indices, species: species, types: types})
   }
 
 
@@ -55,17 +55,17 @@ class Baseroute extends Component {
                      <img src={this.state.selectedPokemon.sprites.front_default}/>
                     <img src={this.state.selectedPokemon.sprites.front_shiny}/>
                     
-                    <h1 class="location">Location</h1>
+                    <h1 class="types">Types</h1>
                     <ul>
-                    {this.state.location.map(l => <p>{l.name}</p>)}
+                    {this.state.types.map(t => <p>{t.name}</p>)}
                     </ul>
-                    <h1 class="type">Type</h1>
+                    <h1 class="species">Species</h1>
                     <ul>
-                    {this.state.type.map(t => <p>{t.name}</p>)}
+                    {this.state.species.map(s => <p>{s.name}</p>)}
                     </ul>
-                    <h1 className="generation">Generation</h1>
+                    <h1 className="game_indices">Game Indices</h1>
                     <ul>
-                    {this.state.generation.map(g => <p>{g.name}</p>)}
+                    {this.state.game_indices.map(g => <p>{g.name}</p>)}
                     </ul>
                     </div>
               }
@@ -74,4 +74,4 @@ class Baseroute extends Component {
   }
 }
 
-  export default Baseroute;
+  export default Baseroute; 
