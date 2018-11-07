cb753aea994684a8134d914c3a9e2fadc695d06f
LukePatrickHasler <39388864+LHasler@users.noreply.github.com>
2018-11-06 19:44:30 -0500

    Create PokeSearch.js

---
+++ PokeSearch.js
@@ -0,0 +1,56 @@
+import React, {Component} from 'react';
+
+class PokeSearch extends Component {
+    constructor() {
+      super();
+      this.state = {
+        pokemon: [],
+        search: "",
+      }
+    }
+  
+    async componentDidMount() {
+      const res = await  fetch(`https://pokeapi.co/api/v2/pokemon/`)
+      const json = await res.json()
+      this.setState({pokemon: json.results})
+    }
+  
+    onSearchChange = event => {
+      this.setState({search: event.target.value})
+    }
+  
+    generateSearchResults = search => {
+      if (search === "") {
+        return []
+      } else {
+        return this.state.pokemon
+          .filter(p => p.name.includes(search))
+          .slice(0, 10)
+      }
+    }
+
+  
+    render() {
+      const results = this.generateSearchResults(this.state.search)
+      return (
+        <div className="App">
+          <div className="search">
+            <input 
+            onChange={this.onSearchChange}
+            type="text"
+            value={this.state.search} />
+            <ul>
+              {results.map(r => 
+                <li  onClick={() => this.selectPokemon(r.name)}>
+                  {r.name}
+                </li>
+              )}
+            </ul>
+          </div>
+  
+        </div>
+      );
+    }
+  }
+
+  export default PokeSearch; 
