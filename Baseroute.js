c6bd9ccd5bd46518bd2c96b4dc4854782dcba082
LukePatrickHasler <39388864+LHasler@users.noreply.github.com>
2018-11-09 01:24:31 -0500

    Update Baseroute.js

--- Baseroute.js
+++ Baseroute.js
@@ -17,61 +17,58 @@ class Baseroute extends Component {
         const json = await res.json()
 
   
-     const typesPromises = json.types.map(async (t) => {
-      const data = await fetch(t.type.url, {cache: "force-cache"} )
-      const json = await data.json() 
-      return json
+    const typesPromises = json.types.map(async (t) => {
+    const data = await fetch(t.type.url, {cache: "force-cache"} )
+    const json = await data.json() 
+    return json
     })
 
-  const types = await Promise.all(typesPromises)
+      const types = await Promise.all(typesPromises)
   
-  const held_itemsPromises = json.held_items.map(async (h) => {
+    const held_itemsPromises = json.held_items.map(async (h) => {
     const data = await fetch(h.item.url, {cache: "force-cache"} )
     const json = await data.json() 
     return json
-  })
+    })
 
-  const held_items = await Promise.all(held_itemsPromises)
+      const held_items = await Promise.all(held_itemsPromises)
 
-
-  const game_indicesPromises = json.game_indices.map(async (g) => {
+    const game_indicesPromises = json.game_indices.map(async (g) => {
     const data = await fetch(g.version.url, {cache: "force-cache"} )
     const json = await data.json() 
     return json
-  })
+    })
 
-  const game_indices = await Promise.all(game_indicesPromises)
+      const game_indices = await Promise.all(game_indicesPromises)
 
   this.setState({selectedPokemon: json, game_indices: game_indices, types: types, held_items:held_items})
+
   }
 
 
   render() {
       return (
-          <div>
+          <div className="results">
               {this.state.selectedPokemon &&
                     <div className="result">
-                      <img src={this.state.selectedPokemon.sprites.front_default}/>
-                      <img src={this.state.selectedPokemon.sprites.front_shiny}/>
-                        <h1 class="types">Type</h1>
+                      <img className= "sprite" src={this.state.selectedPokemon.sprites.front_default}/>
+                      <img className="shiny" src={this.state.selectedPokemon.sprites.front_shiny}/>
+                        <h1 className="types">Type</h1>
                           <ul>
                             {this.state.types.map(t => <p>{t.name}</p>)}
                           </ul>
-
-                        <h1 className="held_items">Items Held on Capture</h1>
+                        <h1 className="held_items">Items Held on Capture (if any)</h1>
                           <ul>
                             {this.state.held_items.map(h => <p>{h.name}</p>)}
                           </ul>
-
-
-                    <h1 className="game_indices">Game Index</h1>
-                    <ul>
-                    {this.state.game_indices.map(g => <p>{g.name}</p>)}
-                    </ul>
+                        <h1 className="game_indices">Game Index</h1>
+                          <ul>
+                            {this.state.game_indices.map(g => <p>{g.name}</p>)}
+                          </ul>
                     </div>
-              }
-            </div>
-      );
+            }  
+      </div>
+    );
   }
 }
 
